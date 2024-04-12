19f2d5a5df7c4d5b62be51b42a2ce77f9aa4b459
Huber Soto <104796746+hubers0@users.noreply.github.com>
2024-04-12 18:50:40 +0000

    generador de excusas Huber

--- src/app.js
+++ src/app.js
@@ -1,38 +1,48 @@
-/* eslint-disable no-undef */
-let who = ["El perro", "Mi abuela", "Mi tío", "El policia", "Mi profesor"];
-let what = ["se comió", "aplastó", "robó", "rompió", "ensució"];
-let when = [
-  "antes de clases",
-  "justo cuando terminé",
-  "mientras comia",
-  "mientras yo corria"
-];
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
 
-function excuseGenerator(quien, que, cuando) {
-  // eslint-disable-next-line no-undef
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
 
-  numQuien = Math.floor(Math.random() * quien.length);
-  // eslint-disable-next-line no-undef
-  numQue = Math.floor(Math.random() * que.length);
-  // eslint-disable-next-line no-undef
-  numCuando = Math.floor(Math.random() * cuando.length);
-  return (
-    // eslint-disable-next-line no-undef
-    quien[numQuien] +
-    " " +
-    // eslint-disable-next-line no-undef
-    que[numQue] +
-    " " +
-    "mi tarea" +
-    " " +
-    cuando[numCuando]
-  );
-}
-function onLoad() {
-  document.getElementById("excuse").innerHTML = excuseGenerator(
-    who,
-    what,
-    when
-  );
-}
-window.onload = onLoad;
+window.onload = function() {
+  let quien = ["El perro", "Mi abuela", "Mi tío", "El policia", "Mi profesor"];
+
+  let accion = ["se comió", "aplastó", "robó", "rompió", "ensució"];
+
+  let que = ["mi tarea", "mi lap top", "el celular", "mi libro", "la tv"];
+
+  let cuando = [
+    "antes de clases",
+    "justo cuando termine",
+    "mientras comia",
+    "mientras yo corria"
+  ];
+
+  function generarExcusa() {
+    let quienAleatorio = quien[Math.floor(Math.random() * quien.length)];
+    let accionAleatoria = accion[Math.floor(Math.random() * accion.length)];
+    let queAleatoria = que[Math.floor(Math.random() * que.length)];
+    let cuandoAleatoria = cuando[Math.floor(Math.random() * cuando.length)];
+
+    let excusa =
+      quienAleatorio +
+      " " +
+      accionAleatoria +
+      " " +
+      queAleatoria +
+      " " +
+      cuandoAleatoria +
+      ".";
+
+    return excusa;
+  }
+
+  let elementoExcusa = document.getElementById("excuse");
+
+  elementoExcusa.innerHTML = generarExcusa();
+};
+
+document.getElementById("reloadButton").addEventListener("click", function() {
+  location.reload();
+});
